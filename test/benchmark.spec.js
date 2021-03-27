db7e89e29e467971740b672e46b636c57ffc6558
Guo Yunhe <i@guoyunhe.me>
2021-03-28 02:11:56 +0800

    Update test

--- test/benchmark.spec.js
+++ test/benchmark.spec.js
@@ -1,6 +1,6 @@
 describe("DOM", () => {
   it("creates and deletes elements for a million times", () => {
-    for (let i = 0; i < 1000000; i++) {
+    for (let i = 0; i < 100000; i++) {
       const div = document.createElement("div");
       document.body.append(div);
       div.className = "foo bar";
