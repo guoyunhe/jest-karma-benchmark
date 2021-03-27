2edaf8f9374c049225e432ebf123f186b8a78801
Guo Yunhe <i@guoyunhe.me>
2021-03-28 02:10:48 +0800

    Update test

--- test/benchmark.spec.js
+++ test/benchmark.spec.js
@@ -3,6 +3,15 @@ describe("DOM", () => {
     for (let i = 0; i < 1000000; i++) {
       const div = document.createElement("div");
       document.body.append(div);
+      div.className = "foo bar";
+      div.innerHTML =
+        '<span class="first">f<strong>o</strong>o</span><span>bar</span>';
+      div.addEventListener("click", function () {
+        const span = this.querySelector(".first strong");
+        span.classList.toggle("active");
+      });
+      div.click();
+      div.click();
       div.remove();
     }
   });
